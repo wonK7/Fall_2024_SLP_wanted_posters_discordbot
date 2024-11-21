48063237dbed8390a890c65168991dd979d208d1
wonK7 <poociki@gmail.com>
2024-11-21 08:10:38 -0800

    Changed discord token
    I tried to make some changes and edits and then push them, but GitHub is a bit challenging to use for me since it's my first time.

---
+++ wanted_posters_bot_Fall_SLP.py
@@ -0,0 +1,32 @@
+import nextcord
+from nextcord.ext import commands
+from PIL import Image
+from io import BytesIO
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="!", intents=intents)
+
+@client.command()
+async def wanted(ctx, user: nextcord.User = None):
+    if user == None:
+        user = ctx.author
+    print("Bot is running") # for debugging
+
+    wanted = Image.open("wanted.jpg") # open wanted picture
+
+    data = BytesIO(await user.display_avatar.read())
+    profilePicture = Image.open(data) # get discord profile picture
+
+    profilePicture = profilePicture.resize((233, 268)) # picture size
+    wanted.paste(profilePicture, (150, 189)) #picture x, y derection
+
+    wanted.save("profile.jpg") # saved the discord profile picture onto the wanted picture
+
+    await ctx.reply(file = nextcord.File ("profile.jpg")) # !wanted output
+
+
+client.run('token')
+
+
+
